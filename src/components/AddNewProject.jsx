0f8c84b8714d22d0aa726cc35d5beaab35a76c7b
Edy S <edysoto56@gmail.com>
2025-01-21 23:18:58 -0600

    Add more buttons and input field

--- src/components/AddNewProject.jsx
+++ src/components/AddNewProject.jsx
@@ -1,7 +1,6 @@
 export default function AddNewProject() { 
   return (
-    <div className="w-2/3 max-w-xl mx-auto px-4 py-12">
-      <h2 className="text-xl font-bold">Add New Project</h2>
+    <div className="w-2/3 max-w-xl mx-auto px-4 py-12">      
       <form className="flex flex-col space-y-2">
         <label htmlFor="project-name">Project Name</label>
         <input
@@ -16,9 +15,21 @@ export default function AddNewProject() {
           name="project-description"
           className="border-2 border-gray-300 rounded-md p-2"
         ></textarea>
-        <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800">
-          Add Project
-        </button>
+        <label htmlFor="project-due-date">Project Due Date</label>
+        <input
+          type="date"
+          id="project-due-date"
+          name="project-due-date"
+          className="border-2 border-gray-300 rounded-md p-2"
+        />
+        <div className="flex flex-row gap-4 mt-4">
+          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800">
+            Cancel
+          </button>
+          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800">
+            Add Project
+          </button>
+        </div>        
       </form>
     </div>
   );
