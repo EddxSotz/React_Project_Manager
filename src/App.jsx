b019f89ab2cea688f22819b0105dd75e4276c36a
Edy S <edysoto56@gmail.com>
2025-01-23 00:16:41 -0600

    handle new project view, form data capture

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import SideBar from "./components/SideBar";
 import AddNewProject from "./components/AddNewProject";
 
+import { useState } from "react";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId:  undefined,
+    projects: []    
+  });
+
+  const handleAddProject = () => {
+    setProjectState( prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  
+
   return (
     <main className="h-screen w-full flex flex-row">
-      <SideBar />
-      <AddNewProject />
+      <SideBar onClickAddProject={handleAddProject}/>
+      {projectState.selectedProjectId !== undefined ? (
+        <AddNewProject />
+      ) : (
+        <div className="w-2/3 max-w-xl mx-auto px-4 py-12">
+          <h1 className="text-2xl font-bold text-center">Select a project</h1>
+        </div>
+      )}       
     </main>
   );
 }
