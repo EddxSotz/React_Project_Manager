88386012031119010eceb6343703d7782e09900a
Edy S <edysoto56@gmail.com>
2025-01-31 02:06:14 -0600

    Edit projects functionality, minor style changes

--- src/components/EditSingleProject.jsx
+++ src/components/EditSingleProject.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { useRef } from "react";
 
-export default function AddNewProject({ onSaveProject }) { 
-  const projectNameRef = useRef();
-  const projectDescriptionRef = useRef();
-  const projectDueDateRef = useRef();
+export default function EditSingleProject({ projectData, onEditProject, onCancelEdit }) {
 
-  const handleAddProject = () => {    
-    const projectName = projectNameRef.current.value;
-    const projectDescription = projectDescriptionRef.current.value;
-    const projectDueDate = projectDueDateRef.current.value;
+const projectNameRef = useRef();
+const projectDescriptionRef = useRef();
+const projectDueDateRef = useRef();
 
-    onSaveProject({
-      name: projectName,
-      description: projectDescription,
-      dueDate: projectDueDate,
-    })    
-  }
+const handleEditProject = () => {
+  const projectName = projectNameRef.current.value;
+  const projectDescription = projectDescriptionRef.current.value;
+  const projectDueDate = projectDueDateRef.current.value;
 
+  onEditProject({
+    name: projectName,
+    description: projectDescription,
+    dueDate: projectDueDate,
+  });
+}
+
+const handleCancelEdit = () => {
+  onCancelEdit();
+}
   return (
     <div className="w-2/3 max-w-xl mx-auto px-4 py-12"> 
-      <h1 className="text-2xl mb-8 font-bold">Add New Project</h1>     
+      <h1 className="text-2xl mb-8 font-bold">Edit Project</h1>     
       <form className="flex flex-col space-y-2">
         <label htmlFor="project-name">Project Name</label>
         <input
@@ -28,6 +32,7 @@ export default function AddNewProject({ onSaveProject }) {
           id="project-name"
           name="project-name"
           className="border-2 border-gray-300 rounded-md p-2"
+          defaultValue={projectData.name}
           ref={projectNameRef}
           required
         />
@@ -36,6 +41,7 @@ export default function AddNewProject({ onSaveProject }) {
           id="project-description"
           name="project-description"
           className="border-2 border-gray-300 rounded-md p-2"
+          defaultValue={projectData.description}
           ref={projectDescriptionRef}
         ></textarea>
         <label htmlFor="project-due-date">Project Due Date</label>
@@ -44,15 +50,16 @@ export default function AddNewProject({ onSaveProject }) {
           id="project-due-date"
           name="project-due-date"
           className="border-2 border-gray-300 rounded-md p-2"
+          defaultValue={projectData.dueDate}
           ref={projectDueDateRef}
           required
         />
         <div className="flex flex-row gap-4 mt-4">
-          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800">
+          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800" onClick={handleCancelEdit}>
             Cancel
           </button>
-          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800" onClick={handleAddProject}>
-            Add Project
+          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-800" onClick={ handleEditProject }>
+            Save Project
           </button>
         </div>        
       </form>
